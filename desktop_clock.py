3d4cba79eb3475a157685188f3f99375e1b886c7
yaoxuanzhi <yaoxuanzhi@outlook.com>
2024-12-13 22:52:30 +0800

    - 提交初版

---
+++ desktop_clock.py
@@ -0,0 +1,228 @@
+# 参考资料：
+#   - [Animations and Transformations with QtQuick](https://www.pythonguis.com/tutorials/qml-animations-transformations/)
+#   - [在线QML热更编辑器](https://patrickelectric.work/qmlonline/)
+#   - [FigmaQML](https://github.com/mmertama/FigmaQML)
+
+from plugin import *
+from PyQt5.QtQuick import *
+from PyQt5.QtQuickWidgets import *
+from PyQt5.QtQml import *
+from time import localtime
+from resource import *
+
+class Backend(QObject):
+    hms = pyqtSignal(int, int, int, arguments=['hours','minutes','seconds'])
+
+    def __init__(self):
+        super().__init__()
+
+        self.timer = QTimer()
+        self.timer.setInterval(100)  # msecs 100 = 1/10th sec
+        self.timer.timeout.connect(self.update_time)
+        self.timer.start()
+
+    def update_time(self):
+        local_time = localtime()
+        self.hms.emit(local_time.tm_hour, local_time.tm_min, local_time.tm_sec)
+
+class DrawingScene(QGraphicsScene):
+    def __init__(self, runtimePath:str, parent=None):
+        super().__init__(parent)
+
+        qmlPath = os.path.join(runtimePath, "main.qml").replace("/", "\\")
+        qmlUrl = f"file:///{qmlPath}"
+        canvas_quick_item = CanvasQuickQmlItem(QRectF(0, 0, 200, 200), qmlUrl)
+        canvas_quick_item.setFlags(
+            QGraphicsItem.ItemIsSelectable
+            | QGraphicsItem.ItemIsMovable
+            | QGraphicsItem.ItemIsFocusable
+        )
+        canvas_quick_item.setAcceptHoverEvents(True)
+
+        self.backend = Backend()
+        canvas_quick_item.quickWidget.rootObject().setProperty("backend", self.backend)
+        self.backend.update_time()
+        self.addItem(canvas_quick_item)
+
+class DrawingView(QGraphicsView):
+    def __init__(self, scene: QGraphicsScene, parent=None):
+        super().__init__(scene, parent)
+        self.initUI()
+
+    def initUI(self):
+        self.setRenderHints(
+            QPainter.Antialiasing
+            | QPainter.HighQualityAntialiasing
+            | QPainter.TextAntialiasing
+            | QPainter.SmoothPixmapTransform
+        )
+
+        self.setViewportUpdateMode(QGraphicsView.FullViewportUpdate)
+
+        self.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.setStyleSheet(
+            "background: transparent; border:0px; padding: 0px; margin: 0px;"
+        )
+
+        self.scene_width, self.scene_height = 64000, 64000
+        self.scene().setSceneRect(
+            -self.scene_width // 2,
+            -self.scene_height // 2,
+            self.scene_width,
+            self.scene_height,
+        )
+
+        self.setTransformationAnchor(QGraphicsView.AnchorUnderMouse)
+        self.setDragMode(QGraphicsView.RubberBandDrag)
+
+class DesktopClockWindow3(QWidget):
+    def __init__(self, runtimePath:str, parent=None):
+        super().__init__(parent)
+
+        self.setAttribute(Qt.WidgetAttribute.WA_TransparentForMouseEvents, True)
+        self.setWindowFlag(Qt.WindowType.WindowTransparentForInput, True)
+        self.setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground, True)
+        self.setWindowFlags(Qt.WindowType.FramelessWindowHint | Qt.WindowType.WindowStaysOnTopHint | Qt.WindowType.Tool)
+
+        self.runtimePath = runtimePath
+        self.initUI()
+        self.show()
+
+        screen = QApplication.primaryScreen()
+        availableGeometry = screen.availableGeometry()
+
+        self.move(
+            availableGeometry.width() - self.width(), 
+            availableGeometry.height() - self.height(), 
+                  )
+
+    def initUI(self):
+        self.setStyleSheet("QWidget { background-color: #E3212121; }")
+
+        self.layout = QVBoxLayout(self)
+        self.layout.setContentsMargins(0, 0, 0, 0)
+
+        self.scene = DrawingScene(self.runtimePath)
+        view = DrawingView(self.scene)
+
+        self.layout.addWidget(view)
+
+    def paintEvent(self, a0: QPaintEvent) -> None:
+        backgroundPath = QPainterPath()
+        backgroundPath.setFillRule(Qt.WindingFill)
+
+        return super().paintEvent(a0)
+
+class DesktopClockWindow1(QQuickWidget):
+    def __init__(self, runtimePath:str, parent=None):
+        super().__init__(parent)
+
+        qmlPath = runtimePath.replace("/", "\\")
+        qmlUrl = (f"file:///{qmlPath}")
+        self.engine().addImportPath(qmlUrl)
+
+        self.setAttribute(Qt.WidgetAttribute.WA_TransparentForMouseEvents, True)
+        self.setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground, True)
+        self.setWindowFlags(Qt.WindowType.FramelessWindowHint | Qt.WindowType.WindowStaysOnTopHint | Qt.WindowType.Tool)
+        self.setClearColor(QColor(Qt.GlobalColor.transparent))
+
+        qmlPath = os.path.join(runtimePath, "main.qml").replace("/", "\\")
+        qmlUrl = (f"file:///{qmlPath}")
+        self.setSource(QUrl(qmlUrl))
+
+        self.backend = Backend()
+        self.rootObject().setProperty("backend", self.backend)
+        self.backend.update_time()
+
+        screen = QApplication.primaryScreen()
+        availableGeometry = screen.availableGeometry()
+        self.move(
+            availableGeometry.width() - self.width() - 12, 
+            availableGeometry.height() - self.height() - 12, 
+                  )
+
+class DesktopClockWindow2(QQuickView):
+    def __init__(self, runtimePath:str, parent=None):
+        super().__init__(parent)
+
+        self.setFlag(Qt.WindowType.FramelessWindowHint, True)
+        self.setFlag(Qt.WindowType.WindowStaysOnTopHint, True)
+        self.setFlag(Qt.WindowType.WindowTransparentForInput, True)
+        self.setColor(QColor(Qt.GlobalColor.transparent))
+
+        qmlPath = os.path.join(runtimePath, "main.qml").replace("/", "\\")
+        qmlUrl = (f"file:///{qmlPath}")
+        self.setSource(QUrl(qmlUrl))
+
+        self.backend = Backend()
+        self.rootObject().setProperty("backend", self.backend)
+        self.backend.update_time()
+
+        screen = QApplication.primaryScreen()
+        availableGeometry = screen.availableGeometry()
+        self.setPosition(
+            availableGeometry.width() - self.width() - 12, 
+            availableGeometry.height() - self.height() - 48, 
+                  )
+
+
+class DesktopClock(PluginInterface):
+    def __init__(self):
+        super().__init__()
+        self.effectWnd = None
+        self._runtimePath = os.path.dirname(os.path.abspath(__file__))
+        pass
+    
+    @property
+    def runtimePath(self):
+        return self._runtimePath
+
+    @property
+    def previewImages(self) -> list:
+        folderPath = os.path.join(self.runtimePath, "preview")
+        images = glob.glob(f"{folderPath}/*.*", recursive=False)
+        return images
+
+    @property
+    def name(self):
+        return "DesktopClock"
+
+    @property
+    def displayName(self):
+        return "桌面时钟"
+
+    @property
+    def desc(self):
+        return "给桌面右下角添加一个时钟挂件"
+
+    @property
+    def author(self) -> str:
+        return "yaoxuanzhi"
+
+    @property
+    def icon(self):
+        return QIcon(self.runtimePath + "/icons/desktop_clock.svg")
+
+    @property
+    def version(self) -> str:
+        return "v1.0.0"
+
+    @property
+    def url(self) -> str:
+        return "https://github.com/InterwovenCode/desktop_clock"
+
+    @property
+    def tags(self) -> list:
+        return ["clock"]
+
+    def onChangeEnabled(self):
+        if self.enable:
+            QQuickWindow.setSceneGraphBackend(QSGRendererInterface.GraphicsApi.Software)
+            self.effectWnd = DesktopClockWindow1(self.runtimePath)
+            # self.effectWnd = DesktopClockWindow2(self.runtimePath)
+            # self.effectWnd = DesktopClockWindow3(self.runtimePath)
+            self.effectWnd.show()
+        else:
+            self.effectWnd.close()
+            self.effectWnd = None
---
+++ main.qml
@@ -0,0 +1,96 @@
+import QtQuick 2.15
+import QtQuick.Controls 2.15
+import QtGraphicalEffects 1.0
+
+// ApplicationWindow {
+Rectangle {
+    id: root
+    visible: true
+    width: 200
+    height: 200
+    // x: screen.desktopAvailableWidth - width - 12
+    // y: screen.desktopAvailableHeight - height - 48
+    // title: "Clock"
+    // flags: Qt.FramelessWindowHint | Qt.Window
+    property var hms: {'hours': 0, 'minutes': 0, 'seconds': 0 }
+    property QtObject backend
+
+    color: "transparent"
+
+    Image {
+        id: clockface
+        sourceSize.width: parent.width
+        fillMode: Image.PreserveAspectFit
+        source: "qrc:/clock/images/clockface.png"
+
+        Image {
+            x: clockface.width/2 - width/2
+            y: (clockface.height/2) - height/2
+            scale: clockface.width/465
+            source: "qrc:/clock/images/hour.png"
+            transform: Rotation {
+                    origin.x: 12.5; origin.y: 166;
+                    angle: (hms.hours * 30) + (hms.minutes * 0.5)
+            }
+        }
+
+        Image {
+            x: clockface.width/2 - width/2
+            y: clockface.height/2 - height/2
+            source: "qrc:/clock/images/minute.png"
+            scale: clockface.width/465
+            transform: Rotation {
+                    origin.x: 5.5; origin.y: 201;
+                    angle: hms.minutes * 6
+                    Behavior on angle {
+                        SpringAnimation { spring: 1; damping: 0.2; modulus: 360 }
+                    }
+                }
+        }
+
+        Image {
+            x: clockface.width/2 - width/2
+            y: clockface.height/2 - height/2
+            source: "qrc:/clock/images/second.png"
+            scale: clockface.width/465
+            transform: Rotation {
+                    origin.x: 2; origin.y: 202;
+                    angle: hms.seconds * 6
+                    Behavior on angle {
+                        SpringAnimation { spring: 3; damping: 0.2; modulus: 360 }
+                    }
+                }
+        }
+
+        Image {
+            x: clockface.width/2 - width/2
+            y: clockface.height/2 - height/2
+            source: "qrc:/clock/images/cap.png"
+            scale: clockface.width/465
+        }
+    }
+
+    Connections {
+        target: backend
+
+        function onHms(hours, minutes, seconds) {
+            hms = {'hours': hours, 'minutes': minutes, 'seconds': seconds }
+        }
+    }
+
+    MouseArea {
+        anchors.fill: parent
+        property var mousePos: Qt.point(0, 0)
+        property var windowPos: Qt.point(0, 0)
+        
+        onPressed: {
+            mousePos = Qt.point(mouse.x, mouse.y)
+        }
+        
+        onPositionChanged: {
+            windowPos = Qt.point(root.x, root.y)
+            root.x = windowPos.x + (mouse.x - mousePos.x)
+            root.y = windowPos.y + (mouse.y - mousePos.y)
+        }
+    }
+}
